9e395f7c8fb7e381537758a9669ad1fbba41e602
MartinHeinz <martin7.heinz@gmail.com>
2021-01-02 14:31:32 +0100

    Define server fields.

--- api/v1alpha1/server_types.go
+++ api/v1alpha1/server_types.go
@@ -5,6 +5,7 @@ Copyright 2021 Martin Heinz.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -14,22 +15,83 @@ import (
 // ServerSpec defines the desired state of Server
 type ServerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Important: Run "make manifests" to regenerate code after modifying this file
 
-	// Foo is an example field of Server. Edit Server_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// If omitted, defaults to CRD name
+	// +optional
+	ServerName string   `json:"serverName,omitempty"`
+	GameName   GameName `json:"gameName"`
+	Route      string   `json:"route"`
+	// +listType=atomic
+	Port []*int32 `json:"port"`
+
+	// +listType=atomic
+	EnvFrom []corev1.EnvFromSource `json:"envFrom"`
+
+	Storage *ServerStorage `json:"storage,omitempty"`
+
+	// +optional
+	ResourceConstraints *corev1.ResourceRequirements `json:"resourceConstraints,omitempty"`
+}
+
+// +kubebuilder:validation:Enum=Allow;Forbid;Replace
+type GameName string
+
+const (
+	CSGO GameName = "CSGO"
+)
+
+// ServerStorage ...
+// +k8s:openapi-gen=false
+type ServerStorage struct {
+	// +kubebuilder:validation:Pattern=^([+-]?[0-9.]+)([eEinumkKMGTP]*[-+]?[0-9]*)$
+	Size                string                        `json:"size,omitempty"`
+	MountPath           string                        `json:"mountPath,omitempty"`
+	VolumeClaimTemplate *corev1.PersistentVolumeClaim `json:"volumeClaimTemplate,omitempty"`
 }
 
 // ServerStatus defines the observed state of Server
 type ServerStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Important: Run "make manifests" to regenerate code after modifying this file
+
+	// +listType=atomic
+	Conditions []StatusCondition `json:"conditions,omitempty"`
 }
+
+// StatusCondition ...
+// +k8s:openapi-gen=true
+type StatusCondition struct {
+	LastTransitionTime *metav1.Time           `json:"lastTransitionTime,omitempty"`
+	LastUpdateTime     metav1.Time            `json:"lastUpdateTime,omitempty"`
+	Reason             string                 `json:"reason,omitempty"`
+	Message            string                 `json:"message,omitempty"`
+	Status             corev1.ConditionStatus `json:"status,omitempty"`
+	Type               StatusConditionType    `json:"type,omitempty"`
+}
+
+// StatusConditionType ...
+type StatusConditionType string
+
+const (
+	// StatusConditionTypeReconciled ...
+	StatusConditionTypeReconciled StatusConditionType = "Reconciled"
+
+	// StatusConditionTypeDependenciesSatisfied ...
+	StatusConditionTypeDependenciesSatisfied StatusConditionType = "DependenciesSatisfied"
+)
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
 // Server is the Schema for the servers API
+// +k8s:openapi-gen=true
+// +kubebuilder:resource:path=servers,scope=Namespaced
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Reconciled",type="string",JSONPath=".status.conditions[?(@.type=='Reconciled')].status",priority=0,description="Status of the reconcile condition"
+// +kubebuilder:printcolumn:name="Reason",type="string",JSONPath=".status.conditions[?(@.type=='Reconciled')].reason",priority=1,description="Reason for the failure of reconcile condition"
+// +kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[?(@.type=='Reconciled')].message",priority=1,description="Failure message from reconcile condition"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",priority=0,description="Age of the resource"
 type Server struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
